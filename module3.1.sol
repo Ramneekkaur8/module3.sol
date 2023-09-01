840e1a87caaad8bf3c51f9fa7fc8176adad99890
Ramneekkaur8 <138462854+Ramneekkaur8@users.noreply.github.com>
2023-09-01 08:00:17 +0530

    Create module3.1.sol

---
+++ module3.1.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AssertExample {
+    function example(uint256 x) public pure returns (uint256) {
+        assert(x != 0);
+        return 10 / x;
+    }
+}
+
+contract RevertExample {
+    uint256 public balance;
+
+    constructor(uint256 initialBalance) {
+        balance = initialBalance;
+    }
+
+    function withdraw(uint256 amount) public view {
+        require(amount <= balance, "Insufficient balance");
+    }
+}
+
+contract RequireExample {
+    uint256 public balance;
+
+    constructor(uint256 initialBalance) {
+        balance = initialBalance;
+    }
+
+    function withdraw(uint256 amount) public view {
+        require(amount <= balance, "Insufficient balance");
+    }
+}
